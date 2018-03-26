afa4c3718a558f9d810b4b53d9a9a0d7f325a496
Алексей Дурягин <aleksey.duryagin.n@yandex.ru>
2018-03-26 21:47:03 +0300

    aa

--- workspace/Class/src/Pricelist.java
+++ workspace/Class/src/Pricelist.java
@@ -10,44 +10,44 @@ public class Pricelist {
 	
 	
 	public boolean addProduct(Product product) {
-		return priceList.put(product.getCode(), product) == null;
+		return priceList.putIfAbsent(product.getCode(), product) == null;
 	}
 	
 	public void changePrice(long code, Price newPrice) {
-		if (priceList.get(code) == null) throw new NoSuchElementException();
 		Product product = priceList.get(code);
+		if (product == null) throw new NoSuchElementException();
 		priceList.put(code, new Product(code, product.getName(), newPrice));
 	}
 	
 	public void rename(long code, String newName) {
-		if (priceList.get(code) == null) throw new NoSuchElementException();
 		Product product = priceList.get(code);
+		if (product == null) throw new NoSuchElementException();
 		priceList.put(code, new Product(code, newName, product.getPrice()));
 	}
 	
 	public void removeProduct(long code) {
 		if (priceList.remove(code) == null) throw new NoSuchElementException();
-		priceList.remove(code);
 	}
 	
 	public double sum(long code, int quantity) {
-		if (priceList.get(code) == null) throw new NoSuchElementException();
-		double sum = 0.0;
 		Product product = priceList.get(code);
+		if (product == null) throw new NoSuchElementException();
+		double sum = 0.0;
 		sum = quantity *
 				(product.getPrice().getRub() + product.getPrice().getCop() / 100.0);
 		return sum;
 	}
 	
 	 public Price price(long code) {
-		 if (priceList.get(code) == null) throw new NoSuchElementException();
-		 return (priceList.get(code)).getPrice();
+		 Product product = priceList.get(code);
+			if (product == null) throw new NoSuchElementException();
+		 return product.getPrice();
 	 }
 	
 	public String name(long code) {
-		if (priceList.get(code) == null) throw new NoSuchElementException();
-        return priceList.get(code).getName();
+		Product product = priceList.get(code);
+		if (product == null) throw new NoSuchElementException();
+        return product.getName();
     }
-	
 	
 }
